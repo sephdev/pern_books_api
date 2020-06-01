674c3721affc2c80d3037f2a13dcd0967d291c55
Joseph Tandingan <josephdevapps@gmail.com>
2020-06-01 16:22:49 +0800

    set up express server

--- index.js
+++ index.js
@@ -17,3 +17,27 @@ const getBooks = (request, response) => {
     response.status(200).json(results.rows)
   })
 }
+
+const addBook = (request, response) => {
+  const { author, title } = request.body
+
+  pool.query('INSERT INTO books (author, title) VALUES ($1, $2)', [author, title],
+  error => {
+    if (error) {
+      throw error
+    }
+    response.status(201).json({ status: 'success', message: 'Book added.' })
+  })
+}
+
+app
+  .rout('/books')
+  // GET endpoint
+  .get(getBooks)
+  // POST endpoint
+  .post(addBook)
+
+// Start server
+app.listen(process.emitWarning.PORT || 3002, () => {
+  console.log(`Server listening`)
+})
