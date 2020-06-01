d032fef102e69d8a93a977db5d819a196c9b6f31
Joseph Tandingan <josephdevapps@gmail.com>
2020-06-01 16:08:45 +0800

    init commit, create express api, env variables, db init, postgres con

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const { pool } = require('./config')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+
+const getBooks = (request, response) => {
+  pool.query('SELECT * FROM books', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
